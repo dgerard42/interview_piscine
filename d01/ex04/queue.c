e94d240e0f6d715373f2bc576bf21962abbfaf25
dgerard42 <daniella4422@gmail.com>
2018-04-26 11:52:45 -0700

    added a lot of resource files, starting on d02

---
+++ d01/ex04/queue.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgerard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/25 22:58:02 by dgerard           #+#    #+#             */
+/*   Updated: 2018/04/25 22:58:05 by dgerard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+struct s_queue	*queueInit(void)
+{
+
+}
+
+char			*dequeue(struct s_queue *queue)
+{
+
+}
+
+void			enqueue(struct s_queue *queue, char *message)
+{
+
+}
+
+char			*peek(struct s_queue *queue)
+{
+
+}
+
+int				isEmpty(struct s_queue *queue)
+{
+	
+}
